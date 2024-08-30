828d6d6dfd4baf147bca0b66bf48ca655eee270a
Edwin Bwambale <precious@kali>
2024-08-31 00:14:02 +0300

    modified:   101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,12 +1,35 @@
--- Creates a view need_meeting that lists all students that have a score
--- under 80 (strict) and no last_meeting or more than 1 month.
-DROP VIEW IF EXISTS need_meeting;
-CREATE VIEW need_meeting AS
-    SELECT name
-        FROM students
-        WHERE score < 80 AND
-            (
-                last_meeting IS NULL
-                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
-            )
-;
+-- Creates a stored procedure ComputeAverageWeightedScoreForUsers that
+-- computes and store the average weighted score for all students.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+    ALTER TABLE users ADD total_weighted_score INT NOT NULL;
+    ALTER TABLE users ADD total_weight INT NOT NULL;
+
+    UPDATE users
+        SET total_weighted_score = (
+            SELECT SUM(corrections.score * projects.weight)
+            FROM corrections
+                INNER JOIN projects
+                    ON corrections.project_id = projects.id
+            WHERE corrections.user_id = users.id
+            );
+
+    UPDATE users
+        SET total_weight = (
+            SELECT SUM(projects.weight)
+                FROM corrections
+                    INNER JOIN projects
+                        ON corrections.project_id = projects.id
+                WHERE corrections.user_id = users.id
+            );
+
+    UPDATE users
+        SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
+    ALTER TABLE users
+        DROP COLUMN total_weighted_score;
+    ALTER TABLE users
+        DROP COLUMN total_weight;
+END $$
+DELIMITER ;
